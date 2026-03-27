10dae04f4f13bb1bb64c6e74ca3db05124aacd1e
saehyeun <a01072103861@gmail.com>
2026-03-27 09:45:24 +0900

    Add files via upload

---
+++ 0320/Hello.py
@@ -0,0 +1,18 @@
+literal, constant, variable
+operator
+
+control statement
+실행의 순서
+       선택문      if, switch~case
+       반복문      for, while, do~while
+       무시문      break
+       복귀문      return
+
+variable = experssion
+              literal or constant
+              variable
+              operator
+              function or method
+
+년도를 입력받아 윤년(leap year)인지 평년(common year)인지 판별하는 프로그램
+윤년: 4의 배수이고, 100의 배수는 아니고 400의 배수아면 윤년
